007860cc8a75a92ae843d4cf0396ff98fdff2fdf
AntonPlakh <50624101+AntonPlakh@users.noreply.github.com>
2019-05-14 15:48:37 +0300

    Task4

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wvedite slovo");
+            String slovo = Console.ReadLine();
+            int l = slovo.Length;
+
+            bool res=true;
+
+            for (int i = 0; i < l/2; i++)
+            {
+                if (slovo[i] != slovo[l -1- i])
+                {
+                    res = false;
+                    break;
+                }
+            }
+   
+             Console.WriteLine(res);
+            
+
+        }
+    }
+}
